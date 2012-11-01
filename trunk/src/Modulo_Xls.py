ca51074f2ce9216096d79e30d0eb4e2eb9cec9ad
pedrofaisco <pedrofaisco@gmail.com>
2012-11-01 19:19:46 +0000

    filtro dos curso criado inf e comp

--- trunk/src/Modulo_Xls.py
+++ trunk/src/Modulo_Xls.py
@@ -1,3 +1,33 @@
 # -*- coding: utf-8 -*-
-hola!
-22
+from xlrd import open_workbook, cellname, empty_cell
+def read_xls():
+	wb = open_workbook('Inscritos_2010-2011 (formato Excel xls).xls')
+	
+	s = wb.sheet_by_index(30)
+	
+	print 'Sheet:',s.name 
+	print empty_cell.value
+	
+	#file = open( 'tabela.txt' , 'w')
+	uni=''
+	fac=''
+	niv=''
+	cur=''
+
+	for row_index in range(4, s.nrows):
+		cur = s.cell_value(row_index,3)
+		if isinstance(cur, unicode):
+			cur = cur.encode('utf8')
+		if cur.find('Computadores') > 0 or cur.find('Informática') > 0 :
+			for col_index in range(0,2):
+				if s.cell(row_index,col_index).value != empty_cell.value:
+					if col_index==0:
+						uni=s.cell_value(row_index,0)
+						pass
+					if col_index==1:
+						fac=s.cell_value(row_index,1)
+						pass
+					if col_index==2:
+						niv=s.cell_value(row_index,2)
+			print uni, fac, niv, cur, '\n'
+read_xls()
